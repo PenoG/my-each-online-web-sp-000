2ac211189cb50c71f8a6188797f2dad2153da3c4
Mason Phillips <mjphillips2000@gmail.com>
2020-10-12 19:57:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
+
 def my_each(collection)
-  while my_each(collection) do |i| is < 4
-    puts i
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
+  collection
 end
